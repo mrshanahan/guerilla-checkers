e15171ad358999948f316aef0339def8c02f1615
Matt Shanahan <mrshanahan11235@gmail.com>
2023-01-13 21:14:35 -0500

    Reduce monitor.sh timing to 1 second

--- monitor.sh
+++ monitor.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR >/dev/null
-fswatch --exclude ".*" --include "$(pwd)/[^/]+\\.(elm|html|css)$" --include "$(pwd)/src/[^/]+\\.(elm|html|css)$" --event Updated --event Removed --event Created ./ -xrE | go run ./batch.go -i 3 | xargs -0 -L 1 ./build.sh
+fswatch --exclude ".*" --include "$(pwd)/[^/]+\\.(elm|html|css)$" --include "$(pwd)/src/[^/]+\\.(elm|html|css)$" --event Updated --event Removed --event Created ./ -xrE | go run ./batch.go -i 1 | xargs -0 -I '{}' ./build.sh
 popd >/dev/null
