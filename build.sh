7a4c9e99e9549ee3199b4884a5bf03d4d4248920
Matt Shanahan <mrshanahan11235@gmail.com>
2023-01-13 21:14:17 -0500

    Add release build option + fix how builds are copied

--- build.sh
+++ build.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+build_release=0
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -r|--release)
+            build_release=1
+            shift
+            ;;
+        *)
+            echo "[$(date)] error: argument '$1' not recognized" >&2
+            exit 1
+            ;;
+    esac
+done
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR >/dev/null
 echo "[$(date)] Building..."
-elm make ./src/GuerillaCheckers.elm --output build/elm.js >/dev/null
+if [[ $build_release -eq 0 ]]; then
+    elm make ./src/GuerillaCheckers.elm --output build/elm.js >/dev/null
+else
+    elm make ./src/GuerillaCheckers.elm --output build/elm.js --optimize >/dev/null
+fi
 if [[ $? -ne 0 ]]; then
     echo "[$(date)] Build failed; see output above for details"
     popd >/dev/null
@@ -15,7 +33,7 @@ if [[ -d ./dist ]]; then
 fi
 mkdir ./dist
 cp index.html ./dist
-cp -r build ./dist
+cp -r build/* ./dist
 cp style.css ./dist
 echo "[$(date)] Build complete!"
 popd >/dev/null
